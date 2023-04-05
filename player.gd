7cba56cd19c0124040ba53d4cc1dd6d946ce27a0
GowenBailey <116030380+GowenBailey@users.noreply.github.com>
2023-04-05 14:33:57 -0500

    Created Animation

--- player.gd
+++ player.gd
@@ -9,42 +9,52 @@ export(int) var GRAVITY = 4
 export(int) var ADDITIONAL_FALL_GRAVITY = 4
 
 var velocity = Vector2.ZERO
-
+	
 func _physics_process(delta: float) -> void:
-
+	
 	apply_gravity()
 	var input = Vector2.ZERO
 	input.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	
-	if input.x ==0:
+	if input.x == 0:
 		apply_friction()
+		$AnimatedSprite.animation = "Idle"
 	else:
 		apply_acceleration(input.x)
-
+		$AnimatedSprite.animation = "Run"
+		if input.x > 0:
+			$AnimatedSprite.flip_h = true
+		elif input.x < 0:
+			$AnimatedSprite.flip_h = false
+		
 	# player jump
-	if Input.is_action_just_pressed("ui_up"):
-		velocity.y = JUMP_RELEASE_FORCE
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			velocity.y = JUMP_FORCE
 	else:
-		if Input.is_action_just_released("ui_up") and velocity.y < -70:
-			velocity.y = -70
-
+		$AnimatedSprite.animation = "Jump"
+		if Input.is_action_just_released("ui_up") and velocity.y < JUMP_RELEASE_FORCE:
+			velocity.y = JUMP_RELEASE_FORCE
+		
 		if velocity.y > 0:
-			velocity.y += 40
+			velocity.y += ADDITIONAL_FALL_GRAVITY
 
-
+	
 	# apply movement to the player
-	velocity = move_and_slide(velocity)
+	var was_in_air = not is_on_floor()
 	velocity = move_and_slide(velocity, Vector2.UP)
+	var just_landed = is_on_floor() and was_in_air
+	if just_landed:
+		$AnimatedSprite.animation = "Run"
+		$AnimatedSprite.frame = 1
 
 func apply_gravity():
 	# gravity
 	velocity.y += GRAVITY
+	velocity.y = min(velocity.y, 300)
 	
 func apply_friction():
-	velocity.x = move_toward(velocity.x, 0, 20)
+	velocity.x = move_toward(velocity.x, 0, FRICTION)
 	
 func apply_acceleration(amount):
-	velocity.x = move_toward(velocity.x, 50 * amount, 20)
+	velocity.x = move_toward(velocity.x, MAX_SPEED * amount, ACCELERATION)
